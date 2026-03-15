9730149eb5aecbbb745b76ec774f4d667d66fe35
MrLukess <111127329+MrLukess@users.noreply.github.com>
2026-03-15 18:31:03 +0200

    Delete build_app.py

--- build_app.py
+++
@@ -1,4 +0,0 @@
-import webview
-URL = 'http://127.0.0.1:5000' # Если есть Pinggy или Render - вставь сюда ту ссылку!
-webview.create_window('Nexus Chat', URL, width=1200, height=800)
-webview.start()
