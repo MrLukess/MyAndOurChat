17db0be9732bc8e28ae97bfc871569dfa283a8b4
MrLukess <111127329+MrLukess@users.noreply.github.com>
2026-03-18 09:51:01 +0200

    Delete build_app.py

--- build_app.py
+++
@@ -1,7 +0,0 @@
-import webview
-
-# ВОТ СЮДА ВСТАВЬ ССЫЛКУ С RENDER (Обязательно с https:// в начале!)
-URL = 'https://maoc-prfv.onrender.com' 
-
-webview.create_window('MyAndOurChat', URL, width=1200, height=800)
-webview.start()
